db289eaa23a8db5d7a8c6acefbb9d1598174259c
talgat55 <lightx_design@yahoo.com>
2021-03-24 22:46:11 +0600

    fix min length password

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
 export const generatePassword = (lengthPass: number = 8) => {
-    let length = lengthPass,
+    let length = lengthPass < 8 ?  8 : lengthPass,
         charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
         retVal = "";
     for (let i = 0, n = charset.length; i < length; ++i) {
