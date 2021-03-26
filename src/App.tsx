260383e7918b8ccfb78dd9f5c5936488befb55d5
talgat55 <lightx_design@yahoo.com>
2021-03-27 00:25:58 +0600

    add options generate pass

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import React, {useState} from 'react';
-import {Container, Input, Button,Label} from 'semantic-ui-react';
+import React, {FormEvent, useState} from 'react';
+import {Container, Input, Button, Label, Checkbox, CheckboxProps} from 'semantic-ui-react';
 import './App.css';
 import {generatePassword} from "./utils";
 
+type TypeLC = boolean | undefined;
+type TypeUC = boolean | undefined;
+type TypeNumber = boolean | undefined;
+type TypeSymbols = boolean | undefined;
+type TypeAC = boolean | undefined;
 const App = () => {
 
     const [lengthPass, setLengthPass] = useState<number>(8);
     const [newPass, setNewPass] = useState('');
+    const [stateLowerCase, setStateLowerCase] = useState<TypeLC>(true);
+    const [stateUpperCase, setStateUpperCase] = useState<TypeUC>(true);
+    const [stateNumber, setStateNumber] = useState<TypeNumber>(true);
+    const [stateSymbols, setStateSymbols] = useState<TypeSymbols>(true);
+    const [stateAC, setStateAC] = useState<TypeAC>(true);
     const handlerGeneratePass = () => {
-        let tempNewPass = generatePassword(lengthPass);
+        let tempNewPass = generatePassword(lengthPass, stateLowerCase,stateUpperCase,stateNumber,stateSymbols,stateAC);
         setNewPass(tempNewPass);
     }
     return (
@@ -30,11 +40,62 @@ const App = () => {
                     <Input
                         value={lengthPass}
                         type="number"
-                        onChange={e =>{
+                        onChange={e => {
                             setLengthPass(parseInt(e.target.value))
                         }}
                     />
                 </div>
+                <div className="block__center">
+                    <Checkbox
+                        label={{children: 'Lowercase Characters'}}
+                        checked={stateLowerCase}
+                        type="checkbox"
+                        onChange={(e:FormEvent<HTMLInputElement>, data:CheckboxProps)  => {
+                            setStateLowerCase(data.checked)
+                        }}
+                    />
+                </div>
+                <div className="block__center">
+                    <Checkbox
+                        label={{children: 'Uppercase  Characters'}}
+                        checked={stateUpperCase}
+                        type="checkbox"
+                        onChange={(e:FormEvent<HTMLInputElement>, data:CheckboxProps) => {
+                            setStateUpperCase(data.checked)
+                        }}
+                    />
+                </div>
+                <div className="block__center">
+                    <Checkbox
+                        label={{children: 'Numbers'}}
+                        checked={stateNumber}
+                        type="checkbox"
+                        onChange={(e:FormEvent<HTMLInputElement>, data:CheckboxProps) => {
+                            setStateNumber(data.checked)
+                        }}
+                    />
+                </div>
+                <div className="block__center">
+                    <Checkbox
+                        label={{children: 'Symbols'}}
+                        checked={stateSymbols}
+                        type="checkbox"
+                        onChange={(e:FormEvent<HTMLInputElement>, data:CheckboxProps) => {
+                            setStateSymbols(data.checked)
+                        }}
+                    />
+                </div>
+                <div className="block__center">
+                    <Checkbox
+                        label={{children: 'Ambiguous Characters'}}
+                        checked={stateAC}
+                        type="checkbox"
+                        onChange={(e:FormEvent<HTMLInputElement>, data:CheckboxProps) => {
+                            setStateAC(data.checked)
+                        }}
+                    />
+                </div>
+
                 <div className="block_second">
                     {
                         newPass.length ?
--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,28 @@
-export const generatePassword = (lengthPass: number = 8) => {
-    let length = lengthPass < 8 ?  8 : lengthPass,
-        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+export const generatePassword = (lengthPass = 8, lc,uc,numbers,symbols,ac) => {
+    let length = lengthPass < 8 ? 8 : lengthPass,
+        charset = "",
         retVal = "";
+
+    if (lc) {
+        charset = charset.concat(charset, 'abcdefghijklmnopqrstuvwxyz');
+    }
+
+    if (uc) {
+        charset = charset.concat(charset, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    }
+
+    if (numbers) {
+        charset = charset.concat(charset, '0123456789');
+    }
+
+    if (symbols) {
+        charset = charset.concat(charset, '@#$%');
+    }
+
+    if (ac) {
+        charset = charset.concat(charset, "{}[]()/'`~,;:.<>");
+    }
+
     for (let i = 0, n = charset.length; i < length; ++i) {
         retVal += charset.charAt(Math.floor(Math.random() * n));
     }
